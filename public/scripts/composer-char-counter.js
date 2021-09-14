e789624fee74c710ec8f40b1d9ab48d59e788135
Juan <juantvaldez85.@gmail.com>
2021-09-14 02:13:34 +0000

    add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+let count = 140;
+
+const counterText = document.getElementById("tweet-text");
+$(document).ready(function () {
+  // --- our code goes here ---
+
+  const textArea = $("textarea#tweet-text");
+
+  $("#tweet-text").on("input", function () {
+    if (textArea.val().length > 0 && count >= 0) {
+      // count =  $("textarea#tweet-text").val().length;
+      count = count - 1;
+      $(".counter").html(count);
+      console.log(textArea.val().length);
+
+      console.log("Length of textarea: " + this.value.length);
+    } else $(".counter").css({ color: "red" });
+  });
+});
