2be1a86022979d5e0e756442974d0d52f4a1d9e0
Juan <juantvaldez85.@gmail.com>
2021-09-16 21:08:59 +0000

    start Dynamic Tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,14 +19,45 @@ $(document).ready(function () {
   };
 
   const createTweetElement = function (tweetObject) {
-    const $tweet = $(
-      `<article class="tweet">Tweet Tweet. I'm a bird. Tweet Tweet.</article>`
-    );
+    const $name = $("<h6>").text(`${tweetData.user.name}`);
+    const $avatars = $("<span>").text(`${tweetData.user.avatars}`);
+    const $handle = $("<h5>").text(`${tweetData.user.handle}`);
+    const $content = $("<p>").text(`${tweetData.content.text}`);
+    const $createAt = $("<h6>").text(`${tweetData.created_at}`);
+
+    const $article = $("<article>").addClass("article-tweet");
+
+    $article.append($name, $avatars, $handle, $content);
+    console.log($article);
     return tweetObject;
   };
 
+  // <section>
+  //   <article class="article-tweet">
+  //     <header class="tweet-header">
+  //       <h5>
+  //         {" "}
+  //         <span class="fas fa-poo"></span>Newton
+  //       </h5>
+  //       <h5>@SirIsaac</h5>
+  //     </header>
+  //     <div class="tweet">
+  //       If I have seen further it is by standing on the shoulders of giants
+  //     </div>
+  //     <footer>
+  //       <h6> 10 days ago</h6>
+  //       <h6 class="icons-flex">
+  //         <span class="fas fa-flag icon"></span>
+  //         <span class="fas fa-retweet icon"></span>
+  //         <span class="fas fa-heart icon"></span>
+  //       </h6>
+  //     </footer>
+  //   </article>
+  // </section>;
+
   const $tweet = createTweetElement(tweetData);
 
-  console.log($tweet);
+  // console.log($tweet);
+
   $("#tweets-container").append($tweet);
 });
