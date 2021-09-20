19e324a5b369851e6f0993e9e91b07f4eb1a2b8a
Juan <juantvaldez85.@gmail.com>
2021-09-20 20:47:02 +0000

    add refetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,62 +5,40 @@
  */
 
 $(document).ready(function () {
-  // const tweetData = {
-  //   user: {
-  //     name: "Newton",
-  //     avatars: "https://i.imgur.com/73hZDYK.png",
-  //     handle: "@SirIsaac",
-  //   },
-  //   content: {
-  //     text:
-  //       "If I have seen further it is by standing on the shoulders of giants",
-  //   },
-  //   created_at: 1461116232227,
-  // };
-
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      datatype: "json",
+      success: (tweets) => {
+        renderTweets(tweets);
+        // console.log(tweets);
       },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants",
+      error: (error) => {
+        console.log(error);
       },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
+    });
+  };
 
   const renderTweets = function (tweetData) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
 
-    for (let item of tweetData) {
-      console.log(item);
+    const reversedObj = tweetData.reverse();
+
+    $container = $("#tweets-container");
+    $container.empty();
+    for (let item of reversedObj) {
+      // console.log(item);
       const $tweet = createTweetElement(item);
 
-      $container = $("#tweets-container");
       $container.append($tweet);
     }
   };
 
   const createTweetElement = function (tweetObj) {
     const $name = $("<span>").text(" " + `${tweetObj.user.name}`);
-    // const $avatars = $("<span>").text(`${tweetObj.user.avatars}`);
     const $avatars = $(
       `<img src="${tweetObj.user.avatars}" width:"${10}" height:"${25}">`
     );
@@ -82,9 +60,6 @@ $(document).ready(function () {
     const $retweet = $("<span>").addClass("fas fa-retweet icon");
     const $heart = $("<span>").addClass("fas fa-heart icon");
 
-    // uncomment to revert back to working.
-    // $article.append($name, $avatars, $handle, $content, $createdAt);
-
     $article.append(
       $header.append($h5.append($avatars, $name), $handle),
 
@@ -97,7 +72,26 @@ $(document).ready(function () {
     return $article;
   };
 
-  renderTweets(data);
-});
+  const form = $("#createTweetForm");
+  form.on("submit", function (event) {
+    event.preventDefault();
 
-// const $avatars = $("<span>").text(`${tweetData.user.avatars}`);
+    const serializedData = $(this).serialize();
+    // console.log("Serialized Data: " + serializedData);
+    // console.log("Length of tweet: " + serializedData.length);
+
+    if (serializedData.length === 5) {
+      alert("Tweet cannot be empty!");
+    } else if (serializedData.length > 145) {
+      alert("Tweet is either too long!");
+    } else {
+      $.post("/tweets", serializedData).then((res) => {
+        // console.log(event);
+
+        loadTweets();
+      });
+    }
+  });
+
+  loadTweets();
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,13 +7,13 @@ $(document).ready(function () {
   const textArea = $("textarea#tweet-text");
 
   $("#tweet-text").on("input", function () {
-    if (textArea.val().length > 0 && count >= 0) {
+    if (textArea.val().length > 0 && count > 0) {
       // count =  $("textarea#tweet-text").val().length;
       count = count - 1;
       $(".counter").html(count);
-      console.log(textArea.val().length);
+      // console.log(textArea.val().length);
 
-      console.log("Length of textarea: " + this.value.length);
+      // console.log("Length of textarea: " + this.value.length);
     } else $(".counter").css({ color: "red" });
   });
 });
